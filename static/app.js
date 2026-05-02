3903ed35c2f661c08e1e91493b7dcb1c93fe84fa
Ken Perry <sirken@users.noreply.github.com>
2026-05-01 23:52:00 -0600

    Disable arrow keys for map movement (#1)

--- static/app.js
+++ static/app.js
@@ -1,4 +1,4 @@
-const map = L.map('map').setView([48.696, -113.718], 11);
+const map = L.map('map', { keyboard: false }).setView([48.696, -113.718], 11);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
 }).addTo(map);
